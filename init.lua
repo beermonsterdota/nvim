2b5486a28d210560db17cdbe93d2039bff04f288
beermonsterdota <obelikc17@gmail.com>
2026-01-08 18:10:10 +0300

    spaces instead of tabs

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 vim.wo.relativenumber = true
 vim.wo.number = true
+vim.o.expandtab = true   -- Use spaces instead of tabs
+vim.o.shiftwidth = 4     -- Size of an indent
+vim.o.tabstop = 4        -- Number of spaces tabs count for
+vim.o.softtabstop = 4    -- Number of spaces tabs count for in editing mode
